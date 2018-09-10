02dcb89413355977c699136434f4bacc411d7f16
Highlander678 <imaranov@gmail.com>
2018-09-10 13:44:16 +0300

    Перегр. опер =

--- Proj-1/Constructor_class.cpp
+++ Proj-1/Constructor_class.cpp
@@ -1,46 +1,46 @@
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-class Point
-{
-private:
-	int x;
-	int y;
-public:
-	Point(int Pr)
-	{
-		x = 1;
-		y = 1;
-		if (Pr)
-		{
-			this->Print();
-			
-			
-		}
-	}
-
-	Point(int x, int y) 
-	{
-		this->x = x;
-		this->y= y;
-		Print();
-	}
-
-private:
-	 void Print()
-	{
-	 	cout << "X = " << x << "\t" << "Y = " << y << endl;
-
-
-	}
-};
-
-
-
-int main()
-{
-	Point Model_1(1);
-	system("pause");
-}
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//class Point
+//{
+//private:
+//	int x;
+//	int y;
+//public:
+//	Point(int Pr)
+//	{
+//		x = 1;
+//		y = 1;
+//		if (Pr)
+//		{
+//			this->Print();
+//			
+//			
+//		}
+//	}
+//
+//	Point(int x, int y) 
+//	{
+//		this->x = x;
+//		this->y= y;
+//		Print();
+//	}
+//
+//private:
+//	 void Print()
+//	{
+//	 	cout << "X = " << x << "\t" << "Y = " << y << endl;
+//
+//
+//	}
+//};
+//
+//
+//
+//int main()
+//{
+//	Point Model_1(1);
+//	system("pause");
+//}
---
+++ Proj-1/Peregruzka_operatorov.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+
+using namespace std;
+
+class Point
+{
+public:
+
+	Point(int Size);
+	void GetPoint(int Size);
+	bool operator == (static Point & other);
+	~Point();
+
+private:
+	int Size;
+	int *arr;
+};
+
+Point::Point(int Size)
+{
+	this->Size = Size;
+	this->arr = new int[this->Size];
+	for (int i = 0; i < this->Size; i++)
+	{
+		//arr[i] = rand() % 30;
+		arr[i] = i;
+	}
+}
+
+void Point::GetPoint(int Size)
+{
+	for (int i = 0; i < Size; i++)
+	{
+		cout << "arr[" << i << "] = \t" << this->arr[i] << endl;
+	}
+}
+
+bool Point::operator==(static Point & other)
+{
+	int t = 0;
+	int f = 0;
+	for (int i = 0; i < other.Size; i++)
+	{
+		
+		if (this->arr[i] == other.arr[i]) t++;
+		else f++;
+	}
+	if (t > 0 && f > 0) return false;
+	if (f == 0 && t>0) return true;
+	if (t == 0 && f > 0) return false;
+	
+	
+}
+
+Point::~Point()
+{
+	delete[] this->arr;
+}
+
+int main()
+{
+	int Size;
+	cin >> Size;
+	Point a(Size);
+	Point b(Size);
+	setlocale(LC_ALL, "ru");
+	cout << "===================== Массив а =================" << endl;
+	a.GetPoint(Size);
+	cout << "===================== Массив b =================" << endl;
+	b.GetPoint(Size);
+	if (a == b)
+	{
+		cout << "Массивы равны" << endl;
+	}
+	else
+	{
+		cout << "Массивы не равны" << endl;
+	}
+
+	system("pause");
+
+	return 0;
+
+}
