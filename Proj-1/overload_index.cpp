2059210e64a29e2e0b1e37f2a95977dfd18a61eb
Highlander678 <imaranov@gmail.com>
2018-09-11 22:02:19 +0300

    Перегрузка оператора [] friend класса

--- Proj-1/overload_index.cpp
+++ Proj-1/overload_index.cpp
@@ -1,22 +1,47 @@
 #include<iostream>
 using namespace std;
-
+//Перегрузка оператора []
 class Point
 {
 public:
-	
-	Point(Size);
+	int & operator [](int index);
+	void Getarr();
+	Point(int Size);
 	~Point();
+	friend void ChangeSize(Point &obj);
 
 private:
+	int Size;
 	int *arr;
 };
 
-Point::Point(Size)
+int & Point::operator[](int index)
 {
-	
-	 = new int[Size];
 
+	return this->arr[index];
+	
+	
+	// TODO: вставьте здесь оператор return
+}
+
+void Point::Getarr()
+{
+	for (int i = 0; i < this->Size; i++)
+	{
+		cout << "arr[" << i << "] =\t" << this->arr[i] << endl;
+		
+	}
+	cout << "this->Size =\t" << this->Size << endl;
+}
+
+Point::Point(int Size)
+{
+	this->Size = Size;
+	arr = new int[Size];
+	for (int i = 0; i < Size; i++)
+	{
+		arr[i] = rand() % 30;
+	}
 }
 
 Point::~Point()
@@ -24,9 +49,17 @@ Point::~Point()
 	delete[] arr;
 }
 
+void ChangeSize(Point &obj)
+{
+	obj.Size += 1;
+}
 
 int main()
 {
-
+	Point a(3);
+	cout << "a[1] =\t" << a[1] << endl;
+	ChangeSize(a);
+	a.Getarr();
+	system("pause");
 	return 0;
 }
