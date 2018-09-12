0ffd1345b28990172f503608e4e8f8d1f75ccd55
Highlander678 <imaranov@gmail.com>
2018-09-12 23:44:32 +0300

    inner Class Arr of objects

---
+++ Proj-1/innerClass.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//создать внешний Image и внутренний класс Pixel
+//Массив объектов класса
+int main()
+{
+	const int Size = 5;
+	Pixel arr[Size]{
+		Pixel (1,2,3),
+		Pixel (4,5,65)
+	};
+}
