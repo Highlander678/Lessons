bc79718258fa2d2c85e6723da70fc40d52700722
Igor Maranov <imaranov@gmail.com>
2019-02-04 22:31:40 +0300

    Revert "Revert "innerclass""
    This reverts commit 4452fadef03f57d1474da1a753956235520ca0bd.

---
+++ Proj-1/InnerClass.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include<string>
+using namespace std;
+//создать внешний Image и внутренний класс Pixel
+//Массив объектов класса
+class Cap {
+public:
+	Cap()
+	{
+		this->colour = "синий";
+	}
+	string returnCapColour() {
+		return this->colour;
+	}
+private:
+	string colour;
+};
+class Human {
+
+public:
+	void think()
+	{
+		brain.calc();
+	}
+	void ChackCapColour()
+	{
+		cout << "Цвет кепки "  << endl;
+	}
+private:
+	class Brain {
+
+	public:
+		Brain()
+		{
+			x = 0;
+		}
+		void calc()
+		{
+			x++;
+			cout << "Думаю X++ = " << x << endl;
+		}
+	private:
+		int x;
+	};
+	Brain brain;
+	Cap cap;
+
+};
+
+
+int main()
+{
+	Human Alex;
+	setlocale(LC_ALL, "ru");
+	Alex.think();
+	Alex.ChackCapColour();
+	system("pause");
+	return 0;
+}
