e747fad2e09e8ffb63eaf9d4af8b39d6343f5a72
Highlander678 <imaranov@gmail.com>
2019-03-04 13:58:40 +0300

    SQL

---
+++ C#/УрокиПрактика/SQL/SQL/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Каталог = new ADOX.Catalog();
+            try
+            {
+                Каталог.Create("Provider=Microsoft.Jet." +
+                         "OLEDB.4.0;Data Source=new_BD.mdb");
+                MessageBox.Show("База успешно создана");
+            }
+            catch (System.Runtime.InteropServices.COMException Ситуация)
+            {
+
+                MessageBox.Show(Ситуация.Message);
+            }
+        }
+    }
+}
---
+++ C#/УрокиПрактика/SQL/SQL/sql_base_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    public partial class sql_base_1 : Component
+    {
+        public sql_base_1()
+        {
+            InitializeComponent();
+        }
+
+        public sql_base_1(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
