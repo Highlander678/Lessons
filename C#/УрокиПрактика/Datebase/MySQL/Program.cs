5d1a3b2f51ade903158cc72f72eaed3c2a717456
Highlander678 <imaranov@gmail.com>
2019-04-04 17:33:42 +0300

    Word

--- C#/УрокиПрактика/Datebase/MySQL/Program.cs
+++ C#/УрокиПрактика/Datebase/MySQL/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
 
 namespace MySQL
 {
@@ -10,6 +12,8 @@ namespace MySQL
     {
         static void Main(string[] args)
         {
+            Application app = new Application();
+
         }
     }
 }
